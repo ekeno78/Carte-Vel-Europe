435a5189cf6eeeb9e68fa52ec756504edb0e82b8
ekeno78 <126951954+ekeno78@users.noreply.github.com>
2024-04-15 10:42:49 +0200

    itinéraire modifier

--- script.js
+++ script.js
@@ -13,22 +13,22 @@ fetch('data.json')
     // Ajout des marqueurs pour chaque lieu
     var markers = [];
 
-    data.forEach(function(place, index) {
+    data.forEach(function (place, index) {
       var marker = L.marker([place.latitude, place.longitude]).addTo(map);
-      
+
       // Création du contenu du popup avec le nom, l'adresse et le QR code
       var popupContent = '<b>' + place.nom + '</b><br>' + place.adresse + '<br>';
-      
+
       // Ajout de l'image du QR code cliquable
-      popupContent += '<a href="' + place.qrcode + '" target="_blank"><img src="images/qrcode.jpg" alt="Je m`informe ! "></a>';
-      
+      popupContent += '<a href="' + place.qrcode + '" target="_blank"><img src="images/qrcode.jpg" alt="En savoir plus !"></a>';
+
       marker.bindPopup(popupContent);
-      
+
       markers.push(marker);
-  });
-  
-  
-  
+    }); 
+
+
+
 
     // // Création de la route en utilisant Leaflet Routing Machine
     // var waypoints = data.map(place => L.latLng(place.latitude, place.longitude));
@@ -40,5 +40,7 @@ fetch('data.json')
     //     styles: [{ color: '#ed8f67', opacity: 0.8, weight: 6 }]
     //   }
     // }).addTo(map);
+
+
   })
   .catch(error => console.error('Erreur lors du chargement des données JSON:', error));
